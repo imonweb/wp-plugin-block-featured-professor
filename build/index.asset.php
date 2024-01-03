0cd44109b9da30b7b5a76872329f0eba1613314b
Imon <contact@imonweb.co.uk>
2024-01-03 01:02:56 +0000

    loading a list of profs

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-data'), 'version' => '8c0ceecb8b2841590c7a');
--- featured-professor.php
+++ featured-professor.php
@@ -3,8 +3,8 @@
 /*
   Plugin Name: Featured Professor Block Type
   Version: 1.0
-  Author: Your Name Here
-  Author URI: https://www.udemy.com/user/bradschiff/
+  Author: Imon
+  Author URI: https://www.imonweb.co.uk/featured-professor
 */
 
 if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
